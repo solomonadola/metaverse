74f209accdc18a238f0df76ac00e29aa01c39493
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2023-09-04 16:19:32 +0300

    First page compelete

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,24 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <nav>navbar</nav>
+    <div className="absolute w-[50%] inset-0 gradient-01"></div>
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
+      />
+
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+        {" "}
+        METAVERSUS
+      </h2>
+      <img
+        src="/menu.svg"
+        alt=""
+        className="w-[24px] h-[24px] object-contain"
+      />
+    </div>
   </motion.nav>
 );
 
