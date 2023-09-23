7566f201719506e05a33f389458e7e6d6a7746d2
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2023-09-23 18:07:57 +0300

    done upto getstarted and new features

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,11 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from "../styles";
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
+    >
+      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
+    </div>
   </div>
 );
 
